83989ef76eb9a7361e8c98d0963b36dfd796394f
Ashis <ashisjena.96@outlook.com>
2019-04-18 23:44:07 +0530

    update profile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,45 @@
 $(document).ready(function(){
 
     google.charts.load('current', {packages: ['corechart', 'bar', 'timeline']});
-    google.charts.setOnLoadCallback(drawCarrierChart);
     google.charts.setOnLoadCallback(drawExperienceChart);
     
-    function drawCarrierChart(){
-
-        var data = new google.visualization.arrayToDataTable([
-            ['Qualification','%',{ role: 'annotation' }],
-            ['BSE',85.66,'85.66%'],
-            ['CHSE',77.16,'77.16%'],
-            ['B.Tech',77,'77%']
-        ]);
-        var options = {
-            title:'Educational Qualification',
-            hAxis:{
-                title:'Qualification'
-            },
-            vAxis:{
-                title:'Percentage(%)',
-                viewWindow:{
-                    min:0,
-                    max:100
-                }
-            }
-        };
-        var chart = new google.visualization.ColumnChart(document.getElementById('qualificationChart'));
-        chart.draw(data, options);
-    }
-    
     function drawExperienceChart(){
+        //debugger;
+        var chart_width = $('.collapsible').innerWidth()*70/100;
         var container = document.getElementById('experienceChart');
-        var chart = new google.visualization.Timeline(container);
+        
         var data = new google.visualization.arrayToDataTable([
-            ['id','Company','start','end'],
-            ['1','Infosys LTD',new Date(2017,12.07),new Date(2018,09,14)],
-            ['2','Mindfire Solurtions',new Date(2018,09.17),Date.now()],
-        ]);
-        chart.draw(data);
+                ['id','Company','start','end'],
+                ['1','Infosys LTD',new Date(2017,11,18),new Date(2018,08,14)],
+                ['2','Mindfire Solurtions',new Date(2018,08,17),Date.now()],
+            ]);
+        var options = {
+            timeline: { showRowLabels: false },
+            width:chart_width,
+            height:'100%',
+            chartArea: {
+                    top:'15%',
+                    width:'70%',
+                    height:'65%'
+                }
+            };
+        var chart = new google.visualization.Timeline(container);    
+        chart.draw(data,options);    
     }
+
+    $(window).resize(function(){
+        if( $("#experienceChart").find("svg").length == 1) {
+            drawExperienceChart();
+        }
+    });
+
+    $(document).scroll(function () {
+        var nav = $(".nav-wrapper");
+        var scroll = $(window).scrollTop();
+        if(scroll >= 534)
+            nav.removeClass('transparent');
+        else
+            nav.addClass('transparent');
+        
+      });
 });
